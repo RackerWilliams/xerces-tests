d505d296f3253e71e234303aaeedbcdf55d12f13
Jorge L. Williams <jorge.williams@rackspace.com>
2012-06-25 01:16:29 -0500

    Added error handler to example.

--- schema_tests/src/main/java/com/rackspace/xerces/Test.java
+++ schema_tests/src/main/java/com/rackspace/xerces/Test.java
@@ -2,6 +2,8 @@ package com.rackspace.xerces;
 
 import javax.xml.validation.*;
 import javax.xml.transform.stream.*;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXParseException;
 
 public class Test {
    public static void main(String[] args) {
@@ -12,18 +14,36 @@ public class Test {
 
       try {
          System.setProperty ("javax.xml.validation.SchemaFactory:http://www.w3.org/XML/XMLSchema/v1.1", "org.apache.xerces.jaxp.validation.XMLSchema11Factory");
+         EH eh = new EH();
 
          String xsd = args[0];
          String instance = args[1];
 
          SchemaFactory factory = SchemaFactory.newInstance("http://www.w3.org/XML/XMLSchema/v1.1");
          factory.setFeature ("http://apache.org/xml/features/validation/cta-full-xpath-checking", true);
+         factory.setErrorHandler(eh);
 
          Schema s = factory.newSchema(new StreamSource(xsd));
-         s.newValidator().validate(new StreamSource(instance));
+         Validator v = s.newValidator();
+         v.setErrorHandler(eh);
+         v.validate(new StreamSource(instance));
 
       } catch (Exception e) {
          e.printStackTrace();
       }
+   }
+}
+
+class EH implements ErrorHandler {
+   public void error(SAXParseException spe) throws SAXParseException {
+      throw spe;
+   }
+
+   public void fatalError(SAXParseException spe) throws SAXParseException {
+      throw spe;
+   }
+
+   public void warning(SAXParseException spe) throws SAXParseException {
+      throw spe;
    }
 }
