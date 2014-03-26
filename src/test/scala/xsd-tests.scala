91688565c105f77e2dae0d1c11dfd0d2694ae2b9
Jorge L. Williams <jorge.williams@rackspace.com>
2014-03-26 15:40:31 -0500

    Inital test setup.

---
+++ src/test/scala/xsd-tests.scala
@@ -0,0 +1,75 @@
+package com.rackspace.com.papi.components.checker.xsd
+
+import java.io.File
+
+import javax.xml.validation.Schema
+import javax.xml.validation.SchemaFactory
+import javax.xml.transform.stream.StreamSource
+import org.xml.sax.SAXException
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import org.scalatest.exceptions.TestFailedException
+
+import org.scalatest.FunSuite
+
+@RunWith(classOf[JUnitRunner])
+class XSDTestSuite extends FunSuite {
+  def isXMLFile (f : File) = f.getName().endsWith(".xml")
+
+  val useSaxon = (System.getProperty("test.xsd.impl","Xerces") == "Saxon")
+  val factory = {
+    if (useSaxon) {
+      val inst = Class.forName("com.saxonica.jaxp.SchemaFactoryImpl").newInstance.asInstanceOf[SchemaFactory]
+      inst.setProperty("http://saxon.sf.net/feature/xsd-version","1.1")
+      inst
+    } else {
+      val inst =  SchemaFactory.newInstance("http://www.w3.org/XML/XMLSchema/v1.1")
+      //
+      //  Enable CTA full XPath2.0 checking in XSD 1.1
+      //
+      inst.setFeature ("http://apache.org/xml/features/validation/cta-full-xpath-checking", true)
+      inst
+    }
+  }
+
+  val parentTestDir = new File("xsd_tests")
+  val testDirs = parentTestDir.listFiles().toList.filter(f => f.isDirectory)
+
+  testDirs.foreach(d => {
+    val xsdFile = new File(d,d.getName()+".xsd")
+    val xsd = factory.newSchema(xsdFile.toURI.toURL)
+    val goodSamples = new File (d, "good").listFiles().toList.filter(isXMLFile)
+    val badSamples  = new File (d, "bad").listFiles().toList.filter(isXMLFile)
+
+    //
+    //  All good samples should validate
+    //
+    goodSamples.foreach (gs => {
+      test (s"Instance $gs should validate against $xsdFile") {
+        try {
+          xsd.newValidator().validate(new StreamSource(gs))
+        } catch {
+          case se : SAXException => throw new TestFailedException(s"Validation Error on instance document: $gs", se, 4)
+          case unknown : Throwable => throw new TestFailedException ("Unkown validation error! ", unknown, 4)
+        }
+      }
+    })
+
+    //
+    //  All bad samples should fail to validate
+    //
+    badSamples.foreach (bs => {
+      test (s"Instance $bs should *not* validate against $xsdFile") {
+        try {
+          xsd.newValidator().validate(new StreamSource(bs))
+          throw new TestFailedException (s"Expecting $bs to fail validation against $xsdFile but it did not!", 4)
+        } catch {
+          case se : SAXException => // Ignore we are good!
+          case unknown : Throwable => throw new TestFailedException ("Unkown validation error! ", unknown, 4)
+        }
+      }
+    })
+  })
+}
